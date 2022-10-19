1449d5d873c943c125180070d1e0b698113428a9
mavlyutovar <mavlutvo.arturr@gmail.com>
2022-10-19 15:41:23 +0500

    Модель и миграция для таблицы cars

---
+++ database/migrations/2022_10_18_130839_add_cars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('model')->default("Не указано");
+            $table->integer('price')->nullable();
+            $table->string('description')->comment('Описание автомобиля')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
